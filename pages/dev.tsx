5a55a8aba0291e77d0770c5871cf31e0fd50a234
rockgold0911 <rockgold0911@gmail.com>
2022-04-09 12:14:34 +0000

    feat(pages): add dev

---
+++ pages/dev.tsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react'
+import type { GetStaticProps, NextPage } from 'next'
+import Head from 'next/head'
+import Dev from 'UI/Dev'
+
+import { withStaticTranslations } from 'app/i18n/hoc'
+
+export const getStaticProps: GetStaticProps = withStaticTranslations((props) => ({ props }), {
+  namespaces: [].concat(['menu']),
+})
+
+const Page: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Vinci - dev</title>
+        <meta name="description" content="A NFT-backed DeFi Protocol for Boosting Liquidity and Hedging Volatility" />
+      </Head>
+      <Dev />
+    </Fragment>
+  )
+}
+
+export default __DEV__ ? Page : (): any => null
