1c4eb22a0c15123bb2eb145d1bdf2432c773f216
rockgold0911 <rockgold0911@gmail.com>
2022-04-09 14:14:34 +0000

    feat(pages): add dashboard

---
+++ pages/my-dashboard.tsx
@@ -0,0 +1,19 @@
+import type { GetStaticProps, NextPage } from 'next'
+import Layout from 'UI/Layout'
+import Dashboard from 'UI/Dashboard'
+
+import { withStaticTranslations } from 'app/i18n/hoc'
+
+export const getStaticProps: GetStaticProps = withStaticTranslations((props) => ({ props }), {
+  namespaces: [].concat(['menu'], ['my-dashboard', 'lend']),
+})
+
+const Page: NextPage = () => {
+  return (
+    <Layout title="Dashboard">
+      <Dashboard />
+    </Layout>
+  )
+}
+
+export default Page
