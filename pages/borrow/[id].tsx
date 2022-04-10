b8cc685c478e4afc601a16375247dc09f3972c98
rockgold0911 <rockgold0911@gmail.com>
2022-04-10 09:54:11 +0000

    feat(pages): add borrow

---
+++ pages/borrow/[id].tsx
@@ -0,0 +1,49 @@
+import type { GetStaticProps, GetStaticPaths, NextPage } from 'next'
+import Layout from 'UI/Layout'
+import BorrowDetail from 'UI/BorrowDetail'
+
+import { withStaticTranslations } from 'app/i18n/hoc'
+import { NFTs } from 'app/web3/market'
+
+export const getStaticProps: GetStaticProps = withStaticTranslations(
+  (props) => {
+    const { id } = props.params
+    return {
+      props: {
+        ...props,
+        nft: NFTs[typeof id === 'string' ? id : ''],
+      },
+    }
+  },
+  {
+    namespaces: [].concat(['menu'], ['borrow', 'lend', 'borrow-detail']),
+  }
+)
+export const getStaticPaths: GetStaticPaths = function ({ locales }) {
+  const ids = Object.keys(NFTs)
+  const paths = [] as any
+
+  locales.forEach((locale) => {
+    ids.forEach((id) => {
+      paths.push({
+        params: { id },
+        locale,
+      })
+    })
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+const Page: NextPage = () => {
+  return (
+    <Layout title="BorrowDetail">
+      <BorrowDetail />
+    </Layout>
+  )
+}
+
+export default Page
---
+++ pages/borrow/index.tsx
@@ -0,0 +1,19 @@
+import type { GetStaticProps, NextPage } from 'next'
+import Layout from 'UI/Layout'
+import Borrow from 'UI/Borrow'
+
+import { withStaticTranslations } from 'app/i18n/hoc'
+
+export const getStaticProps: GetStaticProps = withStaticTranslations((props) => ({ props }), {
+  namespaces: [].concat(['menu'], ['borrow']),
+})
+
+const Page: NextPage = () => {
+  return (
+    <Layout title="Borrow">
+      <Borrow />
+    </Layout>
+  )
+}
+
+export default Page
