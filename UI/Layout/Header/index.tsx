66c41c4c33eaf8a5b47165d9e205ff95e40a52e4
rockgold0911 <rockgold0911@gmail.com>
2022-04-26 15:18:01 +0800

    feat(UI): add NProgress

--- UI/Layout/Header/index.tsx
+++ UI/Layout/Header/index.tsx
@@ -2,8 +2,8 @@ import type { FC } from 'react'
 import dynamic from 'next/dynamic'
 import { styled } from '@mui/material/styles'
 import Stack from '@mui/material/Stack'
-import ThemeButton from 'app/Theme/ThemeButton'
 
+import ThemeButton from 'app/Theme/ThemeButton'
 import { useMemoEmpty } from 'app/hooks/useMemoEmpty'
 
 import Menu from './Menu'
@@ -12,9 +12,15 @@ import Logo from './Logo'
 const ChainButton = dynamic(() => import('app/wallet/ChainButton'), { ssr: false })
 const ConnectButton = dynamic(() => import('app/wallet/ConnectButton'), { ssr: false })
 const LanguageMenu = dynamic(() => import('app/i18n/components/LanguageMenu'), { ssr: false })
+const NProgress = dynamic(() => import('lib/nprogress/NProgress'), { ssr: false })
 
 const Header: FC = () => {
   const ROOT = useMemoEmpty(
+    () => styled('header')`
+      position: relative;
+    `
+  )
+  const BODY = useMemoEmpty(
     () => styled('header')`
       height: 85px;
       display: flex;
@@ -35,14 +41,17 @@ const Header: FC = () => {
   )
   return (
     <ROOT>
-      <Logo />
-      <Menu />
-      <RIGHT direction="row" spacing={2}>
-        <ChainButton />
-        <ConnectButton />
-        <ThemeButton />
-        <LanguageMenu />
-      </RIGHT>
+      <BODY>
+        <Logo />
+        <Menu />
+        <RIGHT direction="row" spacing={2}>
+          <ChainButton />
+          <ConnectButton />
+          <ThemeButton />
+          <LanguageMenu />
+        </RIGHT>
+      </BODY>
+      <NProgress />
     </ROOT>
   )
 }
