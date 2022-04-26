ab65baf5f0fbae0c5d24c3ac91451941e5a503be
rockgold0911 <rockgold0911@gmail.com>
2022-04-26 15:16:30 +0800

    feat(lib): add NProgress

---
+++ lib/nprogress/NProgress.tsx
@@ -0,0 +1,29 @@
+import type { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import LinearProgress from '@mui/material/LinearProgress'
+
+import { useMemoEmpty } from 'app/hooks/useMemoEmpty'
+import { selectPageProgress } from 'store/progress/page'
+import { PROGRESS_STATUS } from './helpers'
+
+const NProgress: FC = () => {
+  const ROOT = useMemoEmpty(
+    () => styled(Box)`
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+    `
+  )
+  const { value, status } = useSelector(selectPageProgress) || {}
+  if (status === PROGRESS_STATUS.ready) return null
+  console.log(value)
+  return (
+    <ROOT>
+      <LinearProgress variant="determinate" value={value} />
+    </ROOT>
+  )
+}
+
+export default NProgress
