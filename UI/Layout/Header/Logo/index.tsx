2c3b3cab51b2af752fe6ffe54b4786858afb79ac
rockgold0911 <rockgold0911@gmail.com>
2022-04-27 10:48:08 +0800

    feat(UI): add LogoMobile

--- UI/Layout/Header/Logo/index.tsx
+++ UI/Layout/Header/Logo/index.tsx
@@ -1,22 +1,24 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { styled } from '@mui/material/styles'
+import MaterialLink from '@mui/material/Link'
 
 import { useMemoEmpty } from 'app/hooks/useMemoEmpty'
 
 import LogoImg from './images/logo.svg'
 import type { LogoProps } from './types'
+import { RESPONSIVE_DESIGN } from 'styles/constants'
 
 const Logo = (props: LogoProps) => {
   const ROOT = useMemoEmpty(
-    () => styled('a')`
+    () => styled(MaterialLink)`
       display: flex;
       align-items: center;
     `
   )
   return (
     <Link href={__DEV__ ? '/dev' : '/'} passHref>
-      <ROOT>
+      <ROOT sx={RESPONSIVE_DESIGN.width.LESM('100px', '143px')}>
         <Image src={props.imgSrc || LogoImg} alt="Vinci Protocol" />
       </ROOT>
     </Link>
