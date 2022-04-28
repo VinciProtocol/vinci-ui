8d8a57e7ba99069b3bdc98414aee54112dfdc814
rockgold0911 <rockgold0911@gmail.com>
2022-04-28 09:04:47 +0800

    feat(app): ChainDialog adapted to mobile

--- app/wallet/ChainDialog/ChainDialogContent.tsx
+++ app/wallet/ChainDialog/ChainDialogContent.tsx
@@ -1,22 +1,25 @@
 import type { FC } from 'react'
-import { useMemo } from 'react'
+import { useMemo, Fragment } from 'react'
 import { useCallback } from 'react'
 import { styled } from '@mui/material/styles'
 import DialogContent from '@mui/material/DialogContent'
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 
 import { useMemoEmpty } from 'app/hooks/useMemoEmpty'
 import { useWeb3React } from '@web3-react/core'
 import { switchEthereumChain } from 'lib/wallet/utils'
 import { ChainId } from 'app/web3/chain/types'
 import { getNetwork } from 'app/web3/network'
+import { RESPONSIVE_DESIGN } from 'styles/constants'
 
 import ChainIcon from '../ChainIcon'
 import { useWallet } from '..'
 
 const ChainDialogContent: FC = () => {
   const ROOT = useMemoEmpty(() =>
-    styled('div')(({ theme }) => ({
+    styled(Box)(({ theme }) => ({
       display: 'flex',
       justifyContent: 'space-around',
       paddingTop: theme.spacing(1),
@@ -26,12 +29,22 @@ const ChainDialogContent: FC = () => {
     }))
   )
 
-  return (
-    <DialogContent>
-      <ROOT>
+  const buttons = useMemo(
+    () => (
+      <Fragment>
         <ChainButton chainId={ChainId.bsc} />
         <ChainButton chainId={ChainId.kovan} />
-      </ROOT>
+      </Fragment>
+    ),
+    []
+  )
+
+  return (
+    <DialogContent>
+      <ROOT sx={RESPONSIVE_DESIGN.display.GTSM('flex')}>{buttons}</ROOT>
+      <Stack sx={RESPONSIVE_DESIGN.display.LESM('flex')} spacing={2}>
+        {buttons}
+      </Stack>
     </DialogContent>
   )
 }
