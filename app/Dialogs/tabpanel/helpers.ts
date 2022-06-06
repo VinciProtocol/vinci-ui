b087fa8fbad93b80b5051b1d90403cbf1be56eed
rockgold0911 <rockgold0911@gmail.com>
2022-06-06 10:52:18 +0800

    fix: ts error

--- app/Dialogs/tabpanel/helpers.ts
+++ app/Dialogs/tabpanel/helpers.ts
@@ -9,6 +9,7 @@ import type { TabPanelBaseProps } from 'app/hoc/tabs/withTabPanel'
 
 import type { TabValue } from '../constants'
 import { useInputSlider } from '../hooks/tab/useInputSlider'
+import { NFT_IDS } from 'app/web3/market'
 
 const defaultGetIsMax = (value: string, balance: string) => value === balance
 
